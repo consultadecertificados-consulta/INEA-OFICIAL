171f69bb1eb48ae629eb6c8b0ae1851197dab513
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-04-05 09:59:01 -0600

    Update folios.js

--- folios.js
+++ folios.js
@@ -39,7 +39,7 @@ const baseDatos = {
     "5d6e7f89-0a1b-4b2c-3d4e-5f6a7b8c9d05": { nombre: "MA  ESTHER ARAIZA NUÑEZ", nivel: "PRIMARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "ELOTA, SINALOA", id: "1622266225" },
     "6d7e8f90-a1b2-4c3d-4e5f-6a7b8c9d0e16": { nombre: "GONZALO VALENCIA GUILLEN", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "COLIMA, COLIMA", id: "1723266336" },
     "7e8f90a1-b2c3-4d4e-5f6a-7b8c9d0e1f2-": { nombre: "MA. EUGENIA SANDOVAL TAVERA", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "GUADALAJARA, JALISCO", id: "1824266447" },
-    "8f90a1b2-c3d4-4e5f-6a7b-8c9d0e1f2a38": { nombre: "-------", nivel: "PRIMARIA", promedio: "8.0", conclusion: "2026-04-20", emision: "2026-04-30", lugar: "MATAHUALA, SLP", id: "1925266558" },
+    "8f90a1b2-c3d4-4e5f-6a7b-8c9d0e1f2a38": { nombre: "JOSE FELIPE FLORES GUILLEN", nivel: "SECUNDARIA", promedio: "8.2", conclusion: "2025-02-08", emision: "2025-03-03", lugar: "APATZINGÁN, MICHOACÁN", id: "1925266558" },
     "90a1b2c3-d4e5-4f6a-7b8c-9d0e1f2a3b49": { nombre: "........", nivel: "SECUNDARIA", promedio: "9.2", conclusion: "2026-04-22", emision: "2026-05-02", lugar: "APODACA, NL", id: "2026266669" },
     "a1b2c3d4-4e5f-46a7-b8c9-d0e1f2a3b4c0": { nombre: "NIDIA ARACELI LARA", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2026-04-25", emision: "2026-05-05", lugar: "CELAYA, GUANAJUATO", id: "2127266770" },
     "b2c3d4e5-5f6a-47b8-c9d0-e1f2a3b4c5d1": { nombre: "OSCAR IVAN BERNAL", nivel: "PRIMARIA", promedio: "8.3", conclusion: "2026-04-28", emision: "2026-05-08", lugar: "COACALCO, EDO MEX", id: "2228266881" },
