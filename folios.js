f5c2988cc22ef6ff2c8717610619119b09235d97
consultadecertificados-consulta <consultadecertificados@gmail.com>
2026-09-02 08:43:07 -0600

    Add new student record for BRIAN ALEXIS AGUILAR

--- folios.js
+++ folios.js
@@ -52,7 +52,7 @@ const baseDatos = {
     "9d0e1f2a-4b4c-45d6-e7f8-90a1b2c3d0004e966": { nombre: "MARGARITO GARAY VILLEDA", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2019-07-15", emision: "2019-07-20", lugar: "AZCAPOTZALCO,  CDMX", id: "3036267769" },
     "0e1f2a3b-5c5d-46e7-f890-a1b2c3d4e5f056": { nombre: "---------", nivel: "SECUNDARIA", promedio: "9.0", conclusion: "2016-11-15", emision: "2016-11-20", lugar: "ZUMPANGO , ESTADO DE MÉXICO", id: "3137267870" },
      // --- SEGUNDA TANDA DE REGISTROS SIMULADOS (50 ADICIONALES) ---
-    "f7a1b2c3-d4e5-4a6b-c7d8-e9f0a1b2c3d166": { nombre: "--------", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2017-11-15", emision: "2017-11-20", lugar: "PUEBLA, PUEBLA", id: "3238267981" },
+    "f7a1b2c3-d4e5-4a6b-c7d8-e9f0a1b2c0003d166": { nombre: "BRIAN ALEXIS AGUILAR BAUTISTA", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2026-07-15", emision: "2026-07-20", lugar: "EL MARQUÉS,  QUERÉTARO", id: "3238267981" },
     "e6b2c3d4-f5a6-4b7c-d8e9-f0a1b2c3d4e255": { nombre: "--------", nivel: "SECUNDARIA", promedio: "8.2", conclusion: "2015-11-15", emision: "2015-11-20", lugar: "CUAUTITLAN IZCALLI, ESTADO DE MEXICO", id: "3339268092" },
     "d5c3d4e5-a6f7-4c8d-e9f0-a1b2c3d4e5f366": { nombre: "-------", nivel: "SECUNDARIA", promedio: "8.2", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "ZAPOPAN, JALISCO", id: "3440268203" },
     "c4d4e5f6-b7a8-4d9e-f0a1-b2c3d4e5f6a477": { nombre: "----------", nivel: "SECUNDARIA", promedio: "8.5", conclusion: "2025-11-15", emision: "2025-11-20", lugar: "ZAPOPAN, JALISCO", id: "3541268314" },
